8e098ad2fa8990e19b1f3f2e447f9e5bba9ba9a7
icakofisuw242-droid <icakofisuw242@gmail.com>
2026-05-08 22:48:19 +0530

    Update UPSTREAM_REPO to new GitHub repository

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ZXCVBNMASDFGHJKLQWERTYUIOP0786/SilentCloneBot",
+    "https://github.com/icakofisuw242-droid/CloneMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
